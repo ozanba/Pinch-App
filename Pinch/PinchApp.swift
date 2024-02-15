0c2f1943b607e905420851bf91fb62b0a749476e
ozanba <ozanbagiran@gmail.com>
2024-02-15 23:30:24 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ozan Bağıran on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
